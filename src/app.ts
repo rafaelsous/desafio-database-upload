7907b886ab06cb3dc0d6d00e7629c5038d23534e
rafaelsous <rafaelsousa.souza@gmail.com>
2020-09-01 08:30:33 -0300

    :wrench:

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -13,6 +13,7 @@ createConnection();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
@@ -22,8 +23,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
       message: err.message,
     });
   }
-
-  console.error(err);
 
   return response.status(500).json({
     status: 'error',
